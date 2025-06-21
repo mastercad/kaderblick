7ec12c7dfe8ed372142445dfc2f8aa64da6bebd5
Andreas Kempe <andreas.kempe@byte-artist.de>
2025-06-21 14:10:00 +0200

    add missing monolog bundle

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
