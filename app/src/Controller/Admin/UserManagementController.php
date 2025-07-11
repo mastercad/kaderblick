8bfb4cb513eef1418669563a8d018ddb88825af0
Andreas Kempe <andreas.kempe@byte-artist.de>
2025-07-11 21:22:56 +0200

    run php-cs-fixer

--- app/src/Controller/Admin/UserManagementController.php
+++ app/src/Controller/Admin/UserManagementController.php
@@ -15,7 +15,6 @@ use InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Role\RoleHierarchyInterface;
