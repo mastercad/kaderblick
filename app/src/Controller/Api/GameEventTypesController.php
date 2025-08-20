7becbd9dca695f1b7e8d6bdb0cc3390764d2cb79
Andreas Kempe <andreas.kempe@byte-artist.de>
2025-08-20 17:49:49 +0200

    games overview refactored, possibility added to add gameevents for current running games, refactor controllers in general and move all api only controller to api namespace

--- app/src/Controller/Api/GameEventTypesController.php
+++ app/src/Controller/Api/GameEventTypesController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Api;
 
 use App\Entity\GameEventType;
 use Symfony\Component\Routing\Annotation\Route;
--- app/src/Controller/Api/SurfaceTypesController.php
+++ app/src/Controller/Api/SurfaceTypesController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Api;
 
 use App\Entity\SurfaceType;
 use Symfony\Component\Routing\Annotation\Route;
