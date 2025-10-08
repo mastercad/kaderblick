6b37cdbf7006841213f9f7203a3da07068d6be29
Andreas Kempe <andreas.kempe@byte-artist.de>
2025-10-08 21:41:32 +0200

    make how file is located more robust

--- frontend/scripts/write-buildinfo.sh
+++ frontend/scripts/write-buildinfo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 BUILDNUM=$(git rev-parse --short HEAD 2>/dev/null || echo "unknown")
-echo "{ \"build\": \"$BUILDNUM\" }" > ./public/buildinfo.json
+ls -lisa
+echo "{ \"build\": \"$BUILDNUM\" }" > "$(dirname "$0")/../public/buildinfo.json"
